6bf65e88c214e7e13c305014f367945bbd3ca3a0
ElbyFross <51989477+ElbyFross@users.noreply.github.com>
2019-12-23 15:49:22 +0200

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("UniformDataOperator")]
-[assembly: AssemblyDescription("v1.0.9.3")]
+[assembly: AssemblyDescription("v1.0.10.3")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("FOP Volodymyr Podshyvalov")]
 [assembly: AssemblyProduct("UniformDataOperator")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.9.3")]
-[assembly: AssemblyFileVersion("1.2.0.3")]
+[assembly: AssemblyVersion("1.0.10.3")]
+[assembly: AssemblyFileVersion("1.3.0.3")]
